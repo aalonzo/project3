49fadf1b58578954d2917e3f909341c29bc5cc05
Sunny <42776666+Sunny-iwnl@users.noreply.github.com>
2018-11-26 21:45:46 -0600

    Update professor_test.py

--- professor_test.py
+++ professor_test.py
@@ -4,19 +4,22 @@ from professor import *
 class test_Main(unittest.TestCase):
 
     def test_1_valid_input_with_result(self): # search with valid input and returned results
-        prof = Professor("mullins")
+        linkslist = find_professor("mullins")
+        prof = Professor(linkslist[0])
 
         self.assertEqual(prof.rating, "5.0")
         self.assertEqual(prof.difficulty, "4.0")
 
     def test_2_valid_input_with_no_result(self): # search with valid input, but no results
-        prof = Professor("wafflecone")
+        linkslist = find_professor("wafflecone")
+        prof = Professor(linkslist[0])
 
         self.assertIsNone(prof.rating)
         self.assertIsNone(prof.difficulty)
 
     def test_3_invalid_input(self): # search with invalid input (non-alphanumeric input)
-        prof = Professor("/////")
+        linkslist = find_professor("/////")
+        prof = Professor(linkslist[0])
 
         self.assertIsNone(prof.rating)
         self.assertIsNone(prof.difficulty)
@@ -24,12 +27,12 @@ class test_Main(unittest.TestCase):
     def test_4_find_professor_single_result(self):
         result=find_professor("Richard Fitzpatrick"):
         
-        self.assertEqual(result, ["http://www.ratemyprofessors.com/ShowRatings.jsp?tid=1035306"])
+        self.assertEqual(result, ["/ShowRatings.jsp?tid=1035306"])
         
     def test_5_find_professor_multiple_result(self):
         result=find_professor("Wolesensky"):
         
-        self.assertEqual(result, ["http://www.ratemyprofessors.com/ShowRatings.jsp?tid=1865488","http://www.ratemyprofessors.com/AddRating.jsp?tid=2152246"])
+        self.assertEqual(result, ["/ShowRatings.jsp?tid=1865488","/AddRating.jsp?tid=2152246"])
         
     def test_6_find_professor_no_result(self):
         result=find_professor("Wolensensky"):
