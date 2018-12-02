94d6242b1d59f12de5da9852887dfa2ad5563d7c
Sunny <42776666+Sunny-iwnl@users.noreply.github.com>
2018-12-02 17:57:35 -0600

    Update Course.py

--- Course.py
+++ Course.py
@@ -26,14 +26,16 @@ class Course:
                 timeconv=str(int(timetemp1[1:3])-12)
                 time=time+timeconv+":"+timetemp1[3:5]+" PM "
                 
+            time+="to " 
+        
             timetemp2=timeraw[5:9]
             if timetemp2<1200:
-                time=time+timetemp2[5:7]+":"+timetemp2[7:9]+" AM "
+                time=time+timetemp2[5:7]+":"+timetemp2[7:9]+" AM"
             elif timetemp2<1300:
-                time=time+timetemp2[5:7]+":"+timetemp2[7:9]+" PM "
+                time=time+timetemp2[5:7]+":"+timetemp2[7:9]+" PM"
             else:
                 timeconv=str(int(timetemp2[5:7])-12)
-                time=time+timeconv+":"+timetemp2[7:9]+" PM "
+                time=time+timeconv+":"+timetemp2[7:9]+" PM"
             
             return time
             
