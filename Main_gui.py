8d80040656e4edbac38f00710d0bde73b9bf35fd
Raleigh Melancon <raleighmelancon@gmail.com>
2018-12-02 17:14:26 -0600

    made updates
    added new colors

--- Main_gui.py
+++ Main_gui.py
@@ -5,17 +5,17 @@ root.geometry("800x600")
 frame = Frame(root)
 frame.pack()
 
-bottomframe = Frame(root, bg = "blue")
+bottomframe = Frame(root, bg = "#bf5700")
 bottomframe.pack(ipadx=800, ipady = 15, side = BOTTOM)
 
-redbutton = Button(bottomframe, text = "Professors", fg = "green", bg = "green")
+redbutton = Button(bottomframe, text = "Professors", fg = "white", bg = "#f8971f")
 redbutton.pack(ipadx = 10, ipady = 10, side = LEFT, anchor = CENTER)
 
 
-greenbutton = Button(bottomframe, text = "Classes", fg = "green", bg = "green")
+greenbutton = Button(bottomframe, text = "Classes", fg = "white", bg = "#f8971f")
 greenbutton.pack(ipadx = 10, ipady = 10, side = LEFT, anchor = CENTER)
 
-bluebutton = Button(bottomframe, text = "Grades", fg = "green", bg = "green")
+bluebutton = Button(bottomframe, text = "Grades", fg = "white", bg = "#f8971f")
 bluebutton.pack(ipadx=10, ipady = 10, side = LEFT, anchor = CENTER)
 
 root.mainloop()
