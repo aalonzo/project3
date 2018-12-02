9c0cee82269683c4322fdd08e98944550ef5eb2a
NicholasDanielOTD <nicholasdaniel97@hotmail.com>
2018-12-02 17:30:54 -0600

    Update filestorage.py
    This might work?

--- filestorage.py
+++ filestorage.py
@@ -5,7 +5,7 @@ import os.path
 GLOBAL_PATH = os.getcwd()
 
 class Classbook:
-	def __init__(id):
+	def __init__(self, id):
 		if not os.path.exists(GLOBAL_PATH + "/gradebooks/" + id + "/weights.csv"):
 			file = open(self.fp = GLOBAL_PATH + "/gradebooks/" + id + "/weights.csv", "w+")
 			file.close()
@@ -14,19 +14,20 @@ class Classbook:
 			file.close()
 			
 		self.fp = GLOBAL_PATH + "/gradebooks/" + id 
+		self.grades = read("grades")
+		self.weights = read("weights")
 
-
-	def writeweight(name,weight):
+	def writeweight(self, name,weight):
 		file = open(self.fp + "/weights.csv", "a")
 		writer = csv.writer(file)
 		writer.writerow([name,weight])
 		
-	def writegrade(name,category,grade):
+	def writegrade(self, name,category,grade):
 		file = open(self.fp + "/grades.csv", "a")
 		writer = csv.writer(file)
 		writer.writerow([name,category,grade])
 
-	def read(fileoption):
+	def read(self, fileoption):
 		file = open(self.fp, + "/" + fileoption + ".csv")
 		reader = csv.reader(file)
 		output = []
@@ -34,6 +35,26 @@ class Classbook:
 			output.append(row)
 		return output
 		
-
+	def calculate_grade(self):
+		self.grades = read("grades")
+		self.weights = read("weights")
+		weightlist = []
+		calc_grades = []
+		for item in self.weights:
+			weightlist.append(item[0])
+			calc_grades.append([])
+		for item in self.grades:
+			calc_grades[self.weightlist.index(item[1])].append(item[2])
+		final = 0
+		sums = []
+		colsum = 0
+		for cat in calc_grades:
+			for assignment in cat:
+				colsum += assignment
+			sums.append(colsum)
+			colsum = 0
+		for x in range(len(sums)):
+			final += sums[x] * self.weights[x][1]
+		return final
 
 	
