840880d9e467f8633f1f158f5c352ae67f3f82a7
Sunny <42776666+Sunny-iwnl@users.noreply.github.com>
2018-12-05 16:15:31 -0600

    Update Course.py
    Way to create timecode

--- Course.py
+++ Course.py
@@ -27,7 +27,8 @@ class Course:
 
             timetemp1=timeraw[5:9]
             if int(timetemp1)<1200:
-                time=time+timeraw[5:7]+":"+timeraw[7:9]+ " AM "
+                morntemp1=(timeraw[5:7]).remove('0')
+                time=time+morntemp1+":"+timeraw[7:9]+ " AM "
             elif int(timetemp1)<1300:
                 time=time+timeraw[5:7]+":"+timeraw[7:9]+" PM "
             else:
@@ -38,7 +39,8 @@ class Course:
             
             timetemp2=timeraw[9:13]
             if int(timetemp2)<1200:
-                time=time+timeraw[9:11]+":"+timeraw[11:13]+" AM"
+                morntemp2=(timeraw[9:11]).remove('0')
+                time=time+morntemp2+":"+timeraw[11:13]+" AM"
             elif int(timetemp2)<1300: 
                 time=time+timeraw[9:11]+":"+timeraw[11:13]+" PM"
             else:
@@ -46,9 +48,6 @@ class Course:
                 time=time+timeconv+":"+timeraw[11:13]+" PM"
 
             return time
-
-        def changeGP(self,gp):
-            self.gradepoints=gp
         
         def sameDays(self,other):
             timeraw1=str(other.getTimecode())
@@ -73,14 +72,61 @@ class Course:
             timeraw=str(self.timecode)
             return int(timeraw[9:13])
         
-        def compareTimes(self,other):
+        def checkConflicts(self,other):
             if self.sameDays(other):
                 if other.getStartTime()==self.getStartTime():
-                    return False
-                elif other.getStartTime()<=self.getStartTime() and other.getEndTime()>self.getStartTime():
-                    return False
-                elif other.getStartTime()>=self.getStartTime() and other.getStartTime()<self.getEndTime():
-                    return False
-                elif other.getStartTime()==self.getEndTime():
                     return True
-            return True
+                elif other.getStartTime()<=self.getStartTime() and other.getEndTime()>self.getStartTime():
+                    return True
+                elif other.getStartTime()>=self.getStartTime() and other.getStartTime()<self.getEndTime():
+                    return True
+                elif other.getStartTime()==self.getEndTime():
+                    return False
+            return False
+        
+def createTimecode(days,start,end): #Enter days separated by comma, start and end in format "3:30pm" or similar
+    timecode=""
+    days=days.replace(" ","") #supposed to remove commas, spaces, and lowercase everything
+    days=days.lower()
+    daylist=days.split(',')
+    if "monday" in daylist:
+        timecode+="1"
+    if "tuesday" in daylist:
+        timecode+="2"
+    if "wednesday" in daylist:
+        timecode+="3"
+    if "thursday" in daylist:
+        timecode+="4"
+    if "friday" in daylist:
+        timecode+="5"
+    if len(timecode)<5: #makes sure 5 characters in timecode represent days
+        for i in range(5-len(timecode)):
+            timecode+="0"
+    
+    start=start.replace(" ","") #supposed to remove spaces and lowercase everything
+    start=start.lower() 
+    if len(start)<6: #makes sure four characters in timecode represent start
+        start="0"+start
+    start.remove(':')
+    time1=start[0:2]
+    if start[-2:]=="pm":
+        afttime1=int(start[0:2])
+        if afttime1!=12:            
+            afttime1+=12
+        time1=str(afttime1)
+    timecode+=time1+start[2:4]
+
+    end=end.replace(" ","") #supposed to remove spaces and lowercase everything
+    end=end.lower()
+    if len(end)<6: #makes sure four characters in timecode represent end
+        end="0"+end
+    end.remove(':')
+    time2=end[0:2]
+    if end[-2:]=="pm":
+        afttime2=int(end[0:2])
+        if afttime2!=12:            
+            afttime2+=12
+        time2=str(afttime2)
+    timecode+=time2+end[2:4]
+
+    return int(timecode) #timecode should be in format dddddsssseeee as an int
